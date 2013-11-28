5ab4f69ebe5ed679316193724ae1b29cbd50e63d
Oli <oli@live.co.uk>
2013-11-28 16:56:52 +0000

    Feature: CORS support
    Add CORS support and don't allow duplicate tokens.

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var app = require("express")();
 
 var connections = {};
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get("/open/:token", function(req, res) {
   var token = req.params.token;
 
-  connections[token] = res;
+  if (!connections[token]) {
+    connections[token] = res;
+  } else {
+    res.send("Active token conflict", 409);
+    return;
+  }
 
   setTimeout(function() {
     res.send("Timeout", 408);
@@ -22,7 +33,7 @@ app.get("/close/:token", function(req, res) {
     delete connections[token];
     res.send("OK");
   } else {
-    res.send("No matching connection.");
+    res.send("No matching connection");
   }
 });
 
